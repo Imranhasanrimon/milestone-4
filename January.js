642f5d5aad253486c6ea3a2d0db92d08f571bb76
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-29 15:31:22 +0600

    revision starts with variable

--- January.js
+++ January.js
@@ -1,15 +1,2 @@
-// var age = 25;
-// age = 30;
-// console.log(age);
-// let age = 40;
-// age = 30;
-// console.log(age);
-// const age = 20;
-// age = 50;
-// console.log(age);
-// var apple = parseInt('23.25')
-// var apple = parseFloat('23.25')
-// console.log(apple.toFixed(5));
-var mangoe = 0;
-var taka = -200;
-console.log(taka / mangoe);
+var fullName = 'Imran Hasan';
+console.log(fullName);
