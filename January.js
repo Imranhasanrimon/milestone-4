03fddafff7c629db505f5a97aea62095b5f5923a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-29 22:18:51 +0600

    complext condition

--- January.js
+++ January.js
@@ -1,9 +1,19 @@
 // console.log(10 === '10');
 // if else 
-var studyHours = 4;
-if (studyHours >= 7) {
-    console.log('you will get a job very soon');
-}
-else {
-    console.log('you will not get a job');
+// var studyHours = 4;
+// if (studyHours >= 7) {
+//     console.log('you will get a job very soon');
+// }
+// else {
+//     console.log('you will not get a job');
+// }
+
+//--------------complex condition-----------
+const jobStatus = false;
+const savings = 4000;
+const isFinishIba = false;
+if (jobStatus == true || (savings >= 5000 && isFinishIba == false)) {
+    console.log('i will go to home');
+} else {
+    console.log('i dont go');
 }
