665527a7f60f186d6724a887def15d977a14cac4
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-30 12:58:37 +0600

    quiz is done

--- January.js
+++ January.js
@@ -1,2 +1,9 @@
-// ternary operation 
-(5 > 8) ? console.log('five is bigger') : console.log('five is smaller');
+// var num = 10;
+// if (num > 15) {
+//     console.log("Greater than 15");
+// } else if (num > 5) {
+//     console.log("Greater than 5 but less than or equal to 15");
+// } else {
+//     console.log("Less than or equal to 5");
+// }
+let x = 5; if (x < 10) { x = x + 3; } else { x = x - 2; } console.log(x)
