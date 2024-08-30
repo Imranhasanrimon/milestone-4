98e3a6e982ca90fc7dfe756812d5e4d9b0c74b8b
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-30 11:42:21 +0600

    previous lecture practice

--- January.js
+++ January.js
@@ -1,19 +1,10 @@
-// console.log(10 === '10');
-// if else 
-// var studyHours = 4;
-// if (studyHours >= 7) {
-//     console.log('you will get a job very soon');
-// }
-// else {
-//     console.log('you will not get a job');
-// }
-
-//--------------complex condition-----------
-const jobStatus = false;
-const savings = 4000;
-const isFinishIba = false;
-if (jobStatus == true || (savings >= 5000 && isFinishIba == false)) {
-    console.log('i will go to home');
-} else {
-    console.log('i dont go');
+// multiple logical operation 
+let ram = 8;
+let storage = 1000;
+let gpu = true;
+if ((ram >= 12 || gpu) || storage > 1500) {
+    console.log('you can edit');
+}
+else {
+    console.log('low configuration');
 }
