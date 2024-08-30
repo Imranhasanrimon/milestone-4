d5cb9e2cf74444c9706a3e6a0e0c908a2988f9c1
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-30 14:37:17 +0600

    task one is done

--- January.js
+++ January.js
@@ -1,9 +1,21 @@
-// var num = 10;
-// if (num > 15) {
-//     console.log("Greater than 15");
-// } else if (num > 5) {
-//     console.log("Greater than 5 but less than or equal to 15");
-// } else {
-//     console.log("Less than or equal to 5");
-// }
+var num = 10;
+if (num > 15) {
+    console.log("Greater than 15");
+} else if (num > 5) {
+    console.log("Greater than 5 but less than or equal to 15");
+} else {
+    console.log("Less than or equal to 5");
+}
 let x = 5; if (x < 10) { x = x + 3; } else { x = x - 2; } console.log(x)
+
+
+// free drinks 
+let burgerPrice = 400;
+if (burgerPrice >= 500) {
+    let coke = 'free'
+    console.log(coke);
+}
+else {
+    coke = 30;
+    console.log(coke);
+}
