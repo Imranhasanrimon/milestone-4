86f19384218708112226902c34917ac26dd8de52
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-31 10:02:11 +0600

    task four is done

--- task.js
+++ task.js
@@ -18,3 +18,17 @@ const books = ['Master Your Emotions', 'Eat That Frog', 'Animal Firm', 'Javascri
 let message = `${books.includes('Javascript')}, the mentioned book is included`
 console.log(message);
 console.log('third is done_________________________');
+
+//task four______________________________________
+const age = 23;
+const district = 'Bogura';
+const isMarried = false;
+const skills = ['html', 'css', 'js', 'tailwind'];
+const luckyNumbers = [7, 9, 25];
+console.log(Array.isArray(age));
+console.log(Array.isArray(skills));
+console.log(Array.isArray(district));
+console.log(Array.isArray(luckyNumbers));
+console.log('fourth is done_________________________');
+
+//task five______________________________________
