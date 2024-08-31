148ecdbebe2c3ed48c575cac7b65165d2ae976e2
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-31 09:44:29 +0600

    task two is done

--- task.js
+++ task.js
@@ -3,3 +3,12 @@ const fruits = ['apple', 'orange', 'lemon', 'mango', 'lichi']
 console.log(fruits[3]);
 fruits[2] = 'Jambura'
 console.log(fruits);
+console.log('frist is done_________________________');
+
+//task two __________________________________
+const destinations = ['Sylhet', 'Rangamati', 'Bandarban']
+destinations.push("Cox's Bazar")
+destinations.push('Kuakata', 'Sundarban')
+destinations.pop()
+console.log(destinations);
+console.log('second is done_________________________');
