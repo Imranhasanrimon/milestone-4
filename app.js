eef3ded51db95de45bb10d0f2454a6c568186fc1
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-08-31 16:44:51 +0600

    w3schools doccumentation is done

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let person = '54h';
+const cars = ['20', '55', '84', '7896', '7986', '454'];
+console.log(cars);
+// console.log(cars.toString());
+num1 = cars[3]
 
-console.log(isNaN(person));
+console.log(Array.isArray(cars));
+
+
+const bio = {
+    name: 'imran',
+    add: 'Rajshahi'
+}
+
+console.log(Array.isArray(bio));
+
+console.log(cars instanceof Object);
