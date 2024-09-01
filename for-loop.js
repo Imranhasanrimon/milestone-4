83ee8dc8878f3410226ae00403ae9c7062afd2ea
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-01 22:51:23 +0600

    while loop

--- for-loop.js
+++ for-loop.js
@@ -1,4 +1,20 @@
-const numbers = [21, 534, 54, 26, 80, 63];
-for (const num of numbers) {
-    console.log(num);
+//for loop____________________________
+// const numbers = [21, 534, 54, 26, 80, 63];
+// for (const num of numbers) {
+//     console.log(num);
+
+// }
+
+
+const number = 7;
+if (number < 6) {
+    console.log('small number');
+}
+console.log('big num');
+
+//while loop____________________________
+let num = 0;
+while (num < 5) {
+    console.log('loooping.....', num);
+    num++
 }
