0bebf08248e262f09fd76f7122d592d461e0d621
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-02 16:37:24 +0600

    task two is done

--- task3.js
+++ task3.js
@@ -1,5 +1,18 @@
 //_____________For Loop__________
 //tast one---------
-for (let x = 1; x <= 50; x++) {
-    console.log(x, 'I will invest at least 6 hrs every single day for next 60 days!');
+// for (let x = 1; x <= 50; x++) {
+//     console.log(x, 'I will invest at least 6 hrs every single day for next 60 days!');
+// }
+
+//task two-----------
+// for (let x = 61; x <= 100; x++) {
+//     if (x % 2 !== 0) {
+//         console.log(x);
+//     }
+// }
+
+for (let x = 78; x <= 98; x++) {
+    if (x % 2 === 0) {
+        console.log(x);
+    }
 }
