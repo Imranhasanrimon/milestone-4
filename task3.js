a527b5a9c2eb1ec81c5a351901564050c32092e8
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-02 17:51:44 +0600

    for loop home tasks are solved

--- task3.js
+++ task3.js
@@ -1,30 +1,30 @@
 //_____________For Loop__________
 //tast one---------
-// for (let x = 1; x <= 50; x++) {
-//     console.log(x, 'I will invest at least 6 hrs every single day for next 60 days!');
-// }
+for (let x = 1; x <= 50; x++) {
+    console.log(x, 'I will invest at least 6 hrs every single day for next 60 days!');
+}
 
 //task two-----------
-// for (let x = 61; x <= 100; x++) {
-//     if (x % 2 !== 0) {
-//         console.log(x);
-//     }
-// }
+for (let x = 61; x <= 100; x++) {
+    if (x % 2 !== 0) {
+        console.log(x);
+    }
+}
 
-// for (let x = 78; x <= 98; x++) {
-//     if (x % 2 === 0) {
-//         console.log(x);
-//     }
-// }
+for (let x = 78; x <= 98; x++) {
+    if (x % 2 === 0) {
+        console.log(x);
+    }
+}
 
 //task three-----------
-// let ftotal = 0;
-// for (let x = 91; x <= 129; x++) {
-//     if (x % 2 !== 0) {
-//         ftotal += x;
-//     }
-// }
-// console.log('total:', ftotal);
+let ftotal = 0;
+for (let x = 91; x <= 129; x++) {
+    if (x % 2 !== 0) {
+        ftotal += x;
+    }
+}
+console.log('total:', ftotal);
 
 let fsum = 0;
 for (let x = 51; x <= 85; x++) {
@@ -34,4 +34,14 @@ for (let x = 51; x <= 85; x++) {
 }
 console.log('Sum:', fsum);
 
+//task four-----------
+let number = 9;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = ${number * i}`);
+}
+
+//task five-----------
+for (let i = 81; i >= 65; i--) {
+    console.log(i);
+}
 
