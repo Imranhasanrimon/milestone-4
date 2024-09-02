4f73ca6ee2137a0ec5254b6d1f239a5ff12c7480
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-02 20:27:02 +0600

    all home tasks are almost done

--- task3.js
+++ task3.js
@@ -1,47 +1,156 @@
-//_____________For Loop__________
-//tast one---------
-for (let x = 1; x <= 50; x++) {
-    console.log(x, 'I will invest at least 6 hrs every single day for next 60 days!');
-}
+// //_____________For Loop__________
+// //tast one---------
+// for (let x = 1; x <= 60; x++) {
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!');
+// }
 
-//task two-----------
-for (let x = 61; x <= 100; x++) {
-    if (x % 2 !== 0) {
-        console.log(x);
-    }
-}
+// //task two-----------
+// for (let x = 61; x <= 100; x++) {
+//     if (x % 2 !== 0) {
+//         console.log(x);
+//     }
+// }
 
-for (let x = 78; x <= 98; x++) {
-    if (x % 2 === 0) {
-        console.log(x);
-    }
-}
+// for (let x = 78; x <= 98; x++) {
+//     if (x % 2 === 0) {
+//         console.log(x);
+//     }
+// }
+
+// //task three-----------
+// let ftotal = 0;
+// for (let x = 91; x <= 129; x++) {
+//     if (x % 2 !== 0) {
+//         ftotal += x;
+//     }
+// }
+// console.log('total:', ftotal);
+
+// let fsum = 0;
+// for (let x = 51; x <= 85; x++) {
+//     if (x % 2 === 0) {
+//         fsum += x;
+//     }
+// }
+// console.log('Sum:', fsum);
+
+// //task four-----------
+// let number = 9;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+// //task five-----------
+// for (let i = 81; i >= 65; i--) {
+//     console.log(i);
+// }
+
+
+//_____________While Loop__________
+//task one---------
+// let commitment = 1;
+// while (commitment <= 60) {
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!');
+//     commitment++;
+// }
+
+//task two---------
+// let oddNumber = 61;
+// while (oddNumber <= 100) {
+//     if (oddNumber % 2 !== 0) {
+//         console.log(oddNumber);
+//     }
+//     oddNumber++;
+// }
+
+// let evenNumber = 78;
+// while (evenNumber <= 98) {
+//     console.log(evenNumber);
+//     evenNumber++;
+// }
+
+//task three---------
+// let sumoddNumber = 81;
+// let wsum = 0;
+// while (sumoddNumber <= 131) {
+//     if (sumoddNumber % 2 !== 0) {
+//         wsum += sumoddNumber;
+//     }
+//     sumoddNumber++;
+// }
+// console.log(wsum);
+
+// let sumevenNumber = 206;
+// let wtotal = 0;
+// while (sumevenNumber <= 311) {
+//     if (sumevenNumber % 2 === 0) {
+//         wtotal += sumevenNumber;
+//     }
+//     sumevenNumber++;
+// }
+// console.log(wtotal);
+
+//task four---------
+// let mNumber = 5;
+// let start = 1;
+// while (start <= 10) {
+//     console.log(`${mNumber} * ${start} = ${mNumber * start}`);
+//     start++;
+// }
+
+//task five-------
+// let cNumber = 21;
+// while (cNumber >= 15) {
+//     console.log(cNumber);
+//     cNumber--;
+// }
+
+//_____________Break__________
+//task one-----------
+// for (let i = 1; i <= 200; i++) {
+//     if (i === 100) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+//task two---------
+// let bSum = 0;
+// for (let i = 1; i <= 200; i++) {
+//     bSum += i;
+//     if (bSum >= 100) {
+//         break;
+//     }
+// }
+// console.log(bSum);
 
 //task three-----------
-let ftotal = 0;
-for (let x = 91; x <= 129; x++) {
-    if (x % 2 !== 0) {
-        ftotal += x;
-    }
-}
-console.log('total:', ftotal);
+for (let i = 1; i <= 100; i++) {
 
-let fsum = 0;
-for (let x = 51; x <= 85; x++) {
-    if (x % 2 === 0) {
-        fsum += x;
-    }
-}
-console.log('Sum:', fsum);
-
-//task four-----------
-let number = 9;
-for (let i = 1; i <= 10; i++) {
-    console.log(`${number} * ${i} = ${number * i}`);
 }
 
-//task five-----------
-for (let i = 81; i >= 65; i--) {
-    console.log(i);
-}
+
+
+
+
+
+
+
+
+//_____________Continue__________
+//task one-------
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// task two-------
+// for (let i = 55; i <= 85; i = i + 2) {
+//     if (i % 5 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
 
