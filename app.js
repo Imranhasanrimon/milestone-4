191c6285d1c259a5db3bfa7b24e8572e4b609eee
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-03 23:19:15 +0600

    array sorting

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7]
-console.log(numbers);
-const reversed = [];
-for (let i = 6; i >= 0; i--) {
-    reversed.unshift(numbers[i])
-}
-console.log(reversed);
+// const numbers = [1, 2, 3, 4, 5, 6, 7]
+// console.log(numbers);
+// const reversed = [];
+// for (let number of numbers) {
+//     reversed.unshift(number)
+// }
+// console.log(reversed);
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// let arrReversed = [];
+// for (let i = 0; i < arr.length; i++) {
+//     const item = arr[i];
+//     arrReversed.unshift(item)
+// }
+// console.log(arr);
+// console.log(arrReversed);
+const colors = ['red', 'blue', 'areen', 'yellow', 'orange'];
+const sortedColors = colors.sort();
+console.log(colors);
+console.log(sortedColors);
