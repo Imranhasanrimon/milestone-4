917b6c9cc5e71ce9dde40a116f9ea67fd2cdb295
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-03 15:09:27 +0600

    task one is done

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
-const computer = {
-    brand: 'Dell',
-    price: 40000,
-    ssd: '512 GB',
-    ram: '12 GB'
-}
-console.log(computer);
-delete computer.price
-console.log(computer);
+let text = 'imran hasan rimon '
+console.log(text.repeat(50));
---
+++ task4.js
@@ -0,0 +1,10 @@
+//______________Js object_______________
+//task one--------
+const colors = {
+    red: '#ff0000',
+    green: '#00ff00',
+    blue: '#0000ff',
+    'golden rod': '#daa520'
+}
+console.log(colors['golden rod']);
+
