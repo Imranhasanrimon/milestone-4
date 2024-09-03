73dd401921e59a6571545dcd28390f677e756c8f
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-03 16:08:09 +0600

    string taske one is done

--- task4.js
+++ task4.js
@@ -36,9 +36,37 @@ let student2 = {
     city: 'Gaibandha',
     isStudent: true
 };
+console.log(Object.keys(student2).length);
+// or 
 let propNumber = [];
 for (let prop in student2) {
     propNumber.push(prop)
 }
 console.log(propNumber.length);
-console.log(Object.keys(student2).length);
+
+//task five--------
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+}
+for (let prop in myObject) {
+    console.log('Key: ', prop, '| type: ', typeof myObject[prop]);
+}
+console.log('_________end__________');
+
+
+//______________Js string_______________
+//task one--------
+let text = 'this is an apple in a box';
+let count = 0;
+for (let i = 0; i < text.length; i++) {
+    if (text[i] === 'a') {
+        count++;
+    }
+}
+console.log(count);
+//or
+let count2 = text.split('a').length - 1;
+console.log(count2);
