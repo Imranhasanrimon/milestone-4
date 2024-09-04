f17097860b8eea26aa8ab4fe67943dca1f0791e5
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-04 10:15:42 +0600

    count occurrences problem is solved

--- app.js
+++ app.js
@@ -59,14 +59,23 @@
 //     totalPrice += productPrice[prop];
 // }
 // console.log(totalPrice);
-for (let i = 1; i <= 100; i++) {
-    if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log('fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
+// for (let i = 1; i <= 100; i++) {
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+const arr = [55, 46, 84, 55, 86, 46,]
+let count = 0;
+for (let element of arr) {
+    if (element === 55) {
+        count++;
     }
 }
+console.log(count);
