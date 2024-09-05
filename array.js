eda5a6ce7fe7865f005b845ac8d84b73b4dbe4ad
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-05 08:44:15 +0600

    task two is done

--- array.js
+++ array.js
@@ -1,8 +1,18 @@
 // ______________PROBLEM SOLVING_______________ 
 //number-----------1
-const newArray = [1, 3, 8, 2];
+const newArray = [1, 3, 8, 2, 6];
 let sum = 0;
 for (let i of newArray) {
     sum += i;
 }
 console.log(sum);
+
+//number-----------2
+const newArray2 = [1, 3, 8, 2, 6];
+let largestElement = 0;
+for (let i of newArray2) {
+    if (i > largestElement) {
+        largestElement = i;
+    }
+}
+console.log(largestElement);
