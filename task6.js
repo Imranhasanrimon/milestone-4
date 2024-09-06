87d22da8f8eabf9cd0e39d61b5001356ee2e1b13
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-06 10:41:36 +0600

    task two is solved

--- task6.js
+++ task6.js
@@ -6,3 +6,12 @@ function multiplication(num1, num2, num3, num4) {
 }
 const multResult = multiplication(2, 2, 2, 2);
 console.log(multResult);
+
+//task-2____________
+function conditionalMultiplication(number) {
+    if (number % 2 !== 0) {
+        return number * 2;
+    } return number / 2;
+}
+const conditionalMultiplicationResult = conditionalMultiplication(6);
+console.log(conditionalMultiplicationResult);
