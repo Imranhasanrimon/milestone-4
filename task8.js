43cff1b470aa68d4de2e9b727b9498f53df20b97
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-08 12:37:35 +0600

    all home tasks are done

--- task8.js
+++ task8.js
@@ -59,3 +59,24 @@ function findAveragePhonePrice(phonesArray) {
     return totalPhonePrice / phonesArray.length;
 }
 console.log(findAveragePhonePrice(phones));
+
+//task-5____________
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+function totalSalary(employeesArray) {
+    let totalIncrement = 0;
+    let totalStarting = 0;
+    for (let i of employeesArray) {
+        totalIncrement += i.experience * i.increment;
+        totalStarting += i.starting;
+    }
+    const total = totalIncrement + totalStarting
+    return total / 12
+}
+
+console.log(totalSalary(employees));
+
