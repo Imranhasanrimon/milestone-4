763bb606401426139c29b9f5622d467157ea0a2c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-08 18:13:42 +0600

    solving third problem

--- assingment9batch.js
+++ assingment9batch.js
@@ -25,4 +25,23 @@ function checkName(name) {
         return 'Good Name';
     } else { return 'Bad Name' }
 }
-console.log(checkName('Salman'));
+// console.log(checkName('Salman'));
+
+//PROBLEM - 03__________
+function deleteInvalds(array) {
+    let newArray = [];
+    if (!Array.isArray(array)) {
+        return 'Invalid'
+    }
+    for (let i of array) {
+        if (typeof i === 'number' && !isNaN(i)) {
+            newArray.push(i)
+        }
+    }
+    return newArray;
+
+}
+const checkArray = [1, 3, NaN, 5, undefined, null, -20];
+console.log(deleteInvalds(checkArray));
+
+//PROBLEM - 04__________
