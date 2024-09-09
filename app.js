e05860b8f82944a9f5d4845661852457eeaa1f8d
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-09-09 22:33:43 +0600

    array reversed

--- app.js
+++ app.js
@@ -401,15 +401,25 @@
 // }
 // console.log(countVowels('IAfs;alkmran'));
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     }
+//     else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+function reversedArray(array) {
+    let result = [];
+    for (let i of array) {
+        result.unshift(i);
     }
-    else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
-    }
+    return result;
 }
+console.log(reversedArray(vowels));
