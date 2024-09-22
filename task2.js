6db497a9f4eaef46ceadaaa3cdb2ae6856a5d2b5
Imran Hasan <158317547+Imranhasanrimon@users.noreply.github.com>
2024-09-22 19:09:27 +0600

    Update task2.js

--- task2.js
+++ task2.js
@@ -38,3 +38,46 @@ const allDorms = dormitories.concat(dormsInBogura)
 console.log(dormitories);
 console.log(dormsInBogura);
 console.log(allDorms);
+
+/*
+// task one _________________________________
+const fruits = ['apple', 'orange', 'lemon', 'mango', 'lichi']
+console.log(fruits[3]);
+fruits[2] = 'Jambura'
+console.log(fruits);
+console.log('frist is done_________________________');
+
+//task two __________________________________
+const destinations = ['Sylhet', 'Rangamati', 'Bandarban']
+destinations.push("Cox's Bazar")
+destinations.push('Kuakata', 'Sundarban')
+destinations.pop()
+console.log(destinations);
+console.log('second is done_________________________');
+
+//task three _____________________________________
+const books = ['Master Your Emotions', 'Eat That Frog', 'Animal Firm', 'Javascript']
+let message = `${books.includes('Javascript')}, the mentioned book is included`
+console.log(message);
+console.log('third is done_________________________');
+
+//task four______________________________________
+const age = 23;
+const district = 'Bogura';
+const isMarried = false;
+const skills = ['html', 'css', 'js', 'tailwind'];
+const luckyNumbers = [7, 9, 25];
+console.log(Array.isArray(age));
+console.log(Array.isArray(skills));
+console.log(Array.isArray(district));
+console.log(Array.isArray(luckyNumbers));
+console.log('fourth is done_________________________');
+
+//task five______________________________________
+const dormitories = ['al amin adil', 'prottoy vila', 'Purnima', 'motimohol'];
+const dormsInBogura = ['zora chattrabas', 'Saikat']
+const allDorms = dormitories.concat(dormsInBogura)
+console.log(dormitories);
+console.log(dormsInBogura);
+console.log(allDorms);
+*/
